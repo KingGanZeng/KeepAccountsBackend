1fc79cf73505616308bfbfdecd1253c08a94a01f
zenggan <zenggan@xiaomi.com>
2019-03-14 10:41:33 +0800

    [RC] 设置非必填字段

--- api/models.py
+++ api/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 class User(models.Model):
     username = models.CharField(max_length=50)
-    portrait = models.URLField()
+    portrait = models.URLField(required=False)
     uid = models.CharField(max_length=50)
-    budget = models.DecimalField(max_digits=9, decimal_places=2)
-    group_id_array = models.CharField(max_length=255)
+    budget = models.DecimalField(max_digits=9, decimal_places=2, required=False)
+    group_id_array = models.CharField(max_length=255, required=False)
 
     def __str__(self):
         return self.uid
