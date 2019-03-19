333220344b1971933100984e714f3f9f485fca03
Knight Zeng <k642713867@live.com>
2019-03-20 05:20:21 +0800

    数据库字段修改，依赖添加

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from rest_framework_jwt.views import obtain_jwt_token
 
 
-handler404 = views.custom404
 urlpatterns = [
     path('login', obtain_jwt_token),
     path('user', views.UserList.as_view()),
