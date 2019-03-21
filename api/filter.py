951b909a67f7e9fa50936f0aef63167d392b70f6
Knight Zeng <k642713867@live.com>
2019-03-21 15:01:49 +0800

    新建账单

--- api/filter.py
+++ api/filter.py
@@ -3,8 +3,18 @@ from . import models
 
 
 class UserFilter(django_filters.FilterSet):
-    uid = django_filters.CharFilter(field_name=('uid', ))
+    uid = django_filters.CharFilter('uid')
 
     class Meta:
         model = models.User
         fields = ['uid', ]
+
+
+class RecordFilter(django_filters.rest_framework.FilterSet):
+    book_id = django_filters.CharFilter('book_id')
+    create_timestamp = django_filters.DateTimeFilter('create_timestamp', lookup_expr='icontains')
+
+    class Meta:
+        model = models.Record
+        fields = ['book_id', 'create_timestamp']
+
--- api/urls.py
+++ api/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('login', obtain_jwt_token),
     path('user', views.UserList.as_view()),
     path('getBookList', views.BookList.as_view()),
-    path('getRecordData', views.RecordList.as_view()),
     path('user/wechat-auth', views.UserDetail.as_view()),
     path('user/create', views.UserInfo.as_view({'post': 'create'})),
     path('createBook', views.BookCreate.as_view({'post': 'create'})),
+    path('getRecordData', views.RecordList.as_view({'post': 'create', 'get': 'list'})),
 ]
